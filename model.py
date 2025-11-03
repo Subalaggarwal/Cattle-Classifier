a73a0ef9e2b349cb9f16ce22bd60bb9b70e5e672
Subal Aggarwal <aggarwalsubal@gmail.com>
2025-11-03 21:54:13 +0530

    model.py

--- model.py
+++ model.py
@@ -5,16 +5,7 @@ import torch.nn as nn
 from torchvision.models import ResNet50_Weights
 
 def create_resnet50(num_classes, pretrained=True):
-    """
-    Create a ResNet50 model for cattle breed classification.
     
-    Args:
-        num_classes (int): Number of output classes (breeds)
-        pretrained (bool): If True, load pretrained ImageNet weights
-    
-    Returns:
-        model (torch.nn.Module): Customized ResNet50 model
-    """
     if pretrained:
         weights = ResNet50_Weights.IMAGENET1K_V1
         model = models.resnet50(weights=weights)
